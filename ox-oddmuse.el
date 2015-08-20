036bb86e7ab4b4ca3352a8915ada150cf1b78c0f
Marcin Borkowski <mbork@mbork.pl>
2015-08-20 20:50:09 +0200

    Add a template for GPLv3+ Elisp library

---
+++ ox-oddmuse.el
@@ -0,0 +1,34 @@
+;;; ox-oddmuse.el --- Org-mode exporter for Oddmuse
+
+;; Copyright (C) 2015 Marcin 'mbork' Borkowski
+
+;; Author: Marcin Borkowski <mbork@mbork.pl>
+;; Keywords: outlines, hypermedia, calendar, wp
+
+;; This file is NOT part of GNU Emacs.
+
+;; ox-oddmuse.el is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; ox-oddmuse.el is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with ox-oddmuse.el.
+;; If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; A simple Org-mode exporter for the Oddmuse wiki engine (i.e., more
+;; or less the WikiCreole standard).  Written both as a (hopefully)
+;; useful tool and as an example of exporters for the talk during
+;; EmacsConf 2015.
+
+;;; Code:
+
+(provide 'ox-oddmuse)
+
+;;; ox-oddmuse.el ends here
