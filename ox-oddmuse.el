584abc001209cebef6511abcb89460ddeeb84cac
Marcin Borkowski <mbork@mbork.pl>
2015-08-21 19:21:46 +0200

    Add basic exporter structure

--- ox-oddmuse.el
+++ ox-oddmuse.el
@@ -29,6 +29,69 @@
 
 ;;; Code:
 
+(defcustom ox-oddmuse-menu-key ?a
+  "The dispatch key for the Oddmuse exporter in the exporter menu.")
+
+(org-export-define-backend 'oddmuse
+  '((plain-text . org-oddmuse-plain-text)
+    (paragraph . org-oddmuse-paragraph)
+    (headline . org-oddmuse-headline)
+    (section . org-oddmuse-section)
+    (template . org-oddmuse-template))
+  :export-block "ODDMUSE"
+  :menu-entry `(,ox-oddmuse-menu-key "Export to Oddmuse"
+				    ((?O "As buffer" org-oddmuse-export-as-oddmuse)
+				     (?o "As file" org-oddmuse-export-to-oddmuse))))
+
+(defun org-oddmuse-plain-text (text info)
+  "Transcode a TEXT string from Org to Oddmuse.
+TEXT is the string to transcode.  INFO is a plist holding
+contextual information."
+  text)
+
+(defun org-oddmuse-paragraph (paragraph contents info)
+  "Transcode PARAGRAPH element into Oddmuse format.
+CONTENTS is the paragraph contents.  INFO is a plist used as
+a communication channel."
+  contents)
+
+(defun org-oddmuse-headline (headline contents info)
+  "Transcode HEADLINE from Org to Oddmuse.
+CONTENTS is the actual text, INFO is the communication channel."
+  (concat (make-string (org-export-get-relative-level headline info) ?=)
+	  " "
+	  (org-export-data (org-element-property :title headline) info)
+	  "\n"
+	  contents))
+
+(defun org-oddmuse-section (section contents info)
+  "Transcode a SECTION element from Org to Oddmuse.
+CONTENTS holds the contents of the section.  INFO is a plist
+holding contextual information."
+  contents)
+
+(defun org-oddmuse-template (contents info)
+  "Return complete document string after Oddmuse conversion.
+CONTENTS is the transcoded contents string.  INFO is a plist
+holding export options."
+  contents)
+
+(defun org-oddmuse-export-as-oddmuse
+  (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer as an Oddmuse buffer."
+  (interactive)
+  (org-export-to-buffer 'oddmuse "*Org Oddmuse Export*"
+    async subtreep visible-only body-only ext-plist (lambda ()
+						      (when (fboundp #'oddmuse-mode) (oddmuse-mode)))))
+
+(defun org-oddmuse-export-to-oddmuse
+  (&optional async subtreep visible-only body-only ext-plist)
+  "Export current buffer to a Oddmuse file."
+  (interactive)
+  (let ((outfile (org-export-output-file-name ".oddmuse" subtreep)))
+    (org-export-to-file 'oddmuse outfile
+      async subtreep visible-only body-only ext-plist)))
+
 (provide 'ox-oddmuse)
 
 ;;; ox-oddmuse.el ends here
